865aa4cb87b915001d015dea18340d366a213ab2
Anders <github@myl.dk>
2014-01-27 01:44:42 +0100

    Fixed compile break

--- src/Window/ObjectBase.cs
+++ src/Window/ObjectBase.cs
@@ -79,7 +79,7 @@ namespace SFML
         ////////////////////////////////////////////////////////////
         protected void SetThis(IntPtr cPointer)
         {
-			if (value != IntPtr.Zero && !IsInvalid)
+			if (cPointer != IntPtr.Zero && !IsInvalid)
 				throw new ArgumentException("Possible mem leak");
 			this.cPointer = cPointer;
         }
