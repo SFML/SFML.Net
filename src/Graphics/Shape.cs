30e57395b9957e7d08349950a32c95e7c57a1bae
thomas <slusnucky@gmail.com>
2014-05-23 10:58:10 +0200

    Updated documentation for shapes

--- src/Graphics/Shape.cs
+++ src/Graphics/Shape.cs
@@ -79,12 +79,15 @@ namespace SFML
 
             ////////////////////////////////////////////////////////////
             /// <summary>
-            /// Get a point of the shape.
+            /// Get the position of a point
             ///
+            /// The returned point is in local coordinates, that is,
+            /// the shape's transforms (position, rotation, scale) are
+            /// not taken into account.
             /// The result is undefined if index is out of the valid range.
             /// </summary>
             /// <param name="index">Index of the point to get, in range [0 .. PointCount - 1]</param>
-            /// <returns>Index-th point of the shape</returns>
+            /// <returns>index-th point of the shape</returns>
             ////////////////////////////////////////////////////////////
             public abstract Vector2f GetPoint(uint index);
 
