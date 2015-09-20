e4c708f12e7c6824574d7348ebf5e7227599af95
Dejan Geci <dejan.geci@gmail.com>
2015-09-20 13:38:35 +0200

    Moved all common assembly info to SolutionInfo file and removed empty attributes

--- src/Audio/Properties/AssemblyInfo.cs
+++ src/Audio/Properties/AssemblyInfo.cs
@@ -3,12 +3,6 @@ using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("sfml-audio")]
 [assembly: AssemblyDescription("Audio module of the SFML library")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("SFML")]
-[assembly: AssemblyCopyright("Copyright © Laurent Gomila")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
 
 [assembly: ComVisible(false)]
 [assembly: Guid("f16e1d68-cc36-440d-a157-0304e9084ab1")]
--- src/Graphics/Properties/AssemblyInfo.cs
+++ src/Graphics/Properties/AssemblyInfo.cs
@@ -3,12 +3,6 @@ using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("sfml-graphics")]
 [assembly: AssemblyDescription("Graphics module of the SFML library")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("SFML")]
-[assembly: AssemblyCopyright("Copyright © Laurent Gomila")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
 
 [assembly: ComVisible(true)]
 [assembly: Guid("e6bee356-0d84-48db-a8bd-1288092339a8")]
