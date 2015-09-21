381bc73af2bcfd4174efa9614ccda15f1f0c2cb2
Dejan Geci <dejan.geci@gmail.com>
2015-09-21 11:24:48 +0200

    Added SolutionInfo as central versioning point

---
+++ src/SolutionInfo.cs
@@ -0,0 +1,10 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyProduct("SFML")]
+[assembly: AssemblyCopyright("Copyright © Laurent Gomila")]
+
+[assembly: ComVisible(true)]
+
+[assembly: AssemblyVersion("2.3.0")]
+[assembly: AssemblyFileVersion("2.3.0")]
