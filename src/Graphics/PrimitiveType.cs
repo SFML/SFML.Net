d1d8ab6302ce5bbf4fe54ec2b97e8bddcc74beb1
Alec Iverson <adiverso93@gmail.com>
2016-08-17 15:45:50 -0700

    Renamed/Deprecated PrimitiveType values.

--- src/Graphics/PrimitiveType.cs
+++ src/Graphics/PrimitiveType.cs
@@ -1,5 +1,4 @@
 using System;
-using System.Runtime.InteropServices;
 
 namespace SFML
 {
@@ -23,19 +22,31 @@ namespace SFML
             Lines,
 
             /// List of connected lines, a point uses the previous point to form a line
-            LinesStrip,
+			LineStrip,
 
             /// List of individual triangles
             Triangles,
 
-            /// List of connected triangles, a point uses the two previous points to form a triangle
-            TrianglesStrip,
+			/// List of connected triangles, a point uses the two previous points to form a triangle
+			TriangleStrip,
 
             /// List of connected triangles, a point uses the common center and the previous point to form a triangle
-            TrianglesFan,
+            TriangleFan,
 
-            /// List of individual quads
-            Quads
-        }
+			/// List of individual quads
+			Quads,
+
+			/// List of connected lines, a point uses the previous point to form a line
+			[Obsolete]
+			LinesStrip,
+
+			/// List of connected triangles, a point uses the two previous points to form a triangle
+			[Obsolete]
+			TrianglesStrip,
+
+			/// List of connected triangles, a point uses the common center and the previous point to form a triangle
+			[Obsolete]
+			TrianglesFan,
+		}
     }
 }
