291b61729e5f6c9355f0e1b35fd6859369661db0
Alec Iverson <adiverso93@gmail.com>
2016-10-30 12:52:27 -0700

    Add missing Context.Settings
    Context.SetActive() now returns bool for success/failure.
    
    Bumped SolutionInfo.cs version to 2.4.
    
    Matching CSFML commits:
    SFML/CSFML@57eda5f9234a58d45d82f74ca8e4644478cd2118
    SFML/CSFML@52c7de16f0e1d2c5d97e46598161b76cff7c6f18

--- src/SolutionInfo.cs
+++ src/SolutionInfo.cs
@@ -6,5 +6,5 @@ using System.Runtime.InteropServices;
 
 [assembly: ComVisible(true)]
 
-[assembly: AssemblyVersion("2.3.0")]
-[assembly: AssemblyFileVersion("2.3.0")]
+[assembly: AssemblyVersion("2.4.0")]
+[assembly: AssemblyFileVersion("2.4.0")]
