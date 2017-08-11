b977815aae19d0d448be3794332e29c598f2ca38
Francis <super_f_qc@hotmail.com>
2017-08-10 20:58:39 -0400

    Fix Condition in csproj for creating OS Constants
    Now Handling Self Contained Build Targeting different OS
    If We know the RuntimeIdentifier we will put it as the value of the OS
    Constant else the constant will only be defined

--- src/System/SharedLibName.cs
+++ src/System/SharedLibName.cs
@@ -9,6 +9,7 @@ namespace SFML.System
     {
         private static string decorate(String libname)
         {
+
             if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
             {
                 return $"{libname}-2.dll";
@@ -28,22 +29,21 @@ namespace SFML.System
 
         }
 
-#if   __WINDOWS__
+#if   _WINDOWS_
         public const string audio       = "csfml-audio-2.dll";
         public const string graphics    = "csfml-graphics-2.dll";
         public const string system      = "csfml-system-2.dll";
         public const string window      = "csfml-window-2.dll";
-#elif __LINUX__
-        public const string audio       = "libcsfml-audio.so
-        public const string graphics    = "libcsfml-graphics.so
-        public const string system      = "libcsfml-system.so
-        public const string window      = "libcsfml-window.so
-
-#elif __OSX__
-        public const string audio       = "libcsfml-audio.dylib
-        public const string graphics    = "libcsfml-graphics.dylib
-        public const string system      = "libcsfml-system.dylib
-        public const string window      = "libcsfml-window.dylib
+#elif _OSX_
+        public const string audio       = "libcsfml-audio.dylib";
+        public const string graphics    = "libcsfml-graphics.dylib";
+        public const string system      = "libcsfml-system.dylib";
+        public const string window      = "libcsfml-window.dylib";
+#elif _LINUX_
+        public const string audio       = "libcsfml-audio.so";
+        public const string graphics    = "libcsfml-graphics.so";
+        public const string system      = "libcsfml-system.so";
+        public const string window      = "libcsfml-window.so";
 #endif
 
 
