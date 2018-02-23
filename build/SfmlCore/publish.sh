1d469e4493821de73a83bf25bd6cdd4b2eb7d721
Dražen Šoronda <dsoronda@gmail.com>
2018-02-23 18:46:28 +0100

    Fixed project files to allow unsafe in Release configuration. Added publish scripts for Win and Linux

---
+++ build/SfmlCore/publish.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+target_dir=publish_linux
+mkdir $target_dir
+
+# Audio references other projects so they are automatically builder and copied into target_dir
+dotnet publish -c release -o $target_dir ../../src/Audio/sfml-audio_core.csproj
