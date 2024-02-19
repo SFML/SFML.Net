8bfdda1e97b398a9fbca23790c32694fb74f18df
Miron Alexandru <mironalex@hotmail.com>
2024-02-19 19:25:01 +0200

    Implement Context GetFunction & IsExtensionAvailable

--- src/SFML.Window/Context.cs
+++ src/SFML.Window/Context.cs
@@ -35,14 +35,38 @@ namespace SFML.Window
 
         ////////////////////////////////////////////////////////////
         /// <summary>
+        /// Check whether a given OpenGL extension is available.
+        /// </summary>
+        /// <param name="name">Name of the extension to check for</param>
+        /// <returns>True if available, false if unavailable</returns>
+        ////////////////////////////////////////////////////////////
+        public bool IsExtensionAvailable(string name)
+        {
+            return sfContext_isExtensionAvailable(myThis, name);
+        }
+
+        ////////////////////////////////////////////////////////////
+        /// <summary>
         /// Activate or deactivate the context
         /// </summary>
         /// <param name="active">True to activate, false to deactivate</param>
-        /// <returns>true on success, false on failure</returns>
+        /// <returns>True on success, false on failure</returns>
         ////////////////////////////////////////////////////////////
         public bool SetActive(bool active)
         {
             return sfContext_setActive(myThis, active);
+        }
+
+        ////////////////////////////////////////////////////////////
+        /// <summary>
+        /// Get the address of an OpenGL function.
+        /// </summary>
+        /// <param name="name">Name of the function to get the address of</param>
+        /// <returns>Address of the OpenGL function, <see cref="IntPtr.Zero"/> on failure</returns>
+        ////////////////////////////////////////////////////////////
+        public IntPtr GetFunction(string name)
+        {
+            return sfContext_getFunction(myThis, name);
         }
 
         ////////////////////////////////////////////////////////////
@@ -96,7 +120,13 @@ namespace SFML.Window
         private static extern void sfContext_destroy(IntPtr View);
 
         [DllImport(CSFML.window, CallingConvention = CallingConvention.Cdecl), SuppressUnmanagedCodeSecurity]
+        private static extern bool sfContext_isExtensionAvailable(IntPtr View, string name);
+
+        [DllImport(CSFML.window, CallingConvention = CallingConvention.Cdecl), SuppressUnmanagedCodeSecurity]
         private static extern bool sfContext_setActive(IntPtr View, bool Active);
+
+        [DllImport(CSFML.window, CallingConvention = CallingConvention.Cdecl), SuppressUnmanagedCodeSecurity]
+        private static extern IntPtr sfContext_getFunction(IntPtr View, string name);
 
         [DllImport(CSFML.window, CallingConvention = CallingConvention.Cdecl), SuppressUnmanagedCodeSecurity]
         private static extern ContextSettings sfContext_getSettings(IntPtr View);
